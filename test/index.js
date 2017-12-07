b8e3192e3ca4574478101a7a8ac8bae44e454a42
Cameron <cameron@biteable.com>
2017-12-07 16:39:37 +1100

    jsdoc

--- test/index.js
+++ test/index.js
@@ -2,16 +2,35 @@ import { expect } from 'chai'
 import PromiseRace from '../'
 
 describe('PromiseRace', () => {
-  it('resolves to the first resolved promise', async () => {
-    const queue = new PromiseRace()
-  
-    const p1 = new Promise(resolve => setTimeout(resolve(1), 1000))
-    const p2 = new Promise(resolve => setTimeout(resolve(2), 500))
-  
-    queue.append(p1)
-    queue.append(p2)
+  describe('append()', () => {
+    it('resolves when the last promise appended resolves', async () => {
+      const race = new PromiseRace()
 
-    const result = await queue.resolved()
-    expect(result).to.equal(2)
+      const p1 = new Promise(resolve => setTimeout(resolve(1), 250))
+      const p2 = new Promise(resolve => setTimeout(resolve(2), 500))
+
+      let foo = 0
+
+      race.append(p1).then(() => { foo = 1 })
+      race.append(p2).then(() => { foo = 2 })
+
+      await race.resolved()
+      expect(foo).to.equal(2)
+    })
+  })
+
+  describe('resolved()', () => {
+    it('will resolve when the last promise appended resolves', async () => {
+      const race = new PromiseRace()
+
+      const p1 = new Promise(resolve => setTimeout(resolve(1), 250))
+      const p2 = new Promise(resolve => setTimeout(resolve(2), 500))
+
+      race.append(p1)
+      race.append(p2)
+
+      const result = await race.resolved()
+      expect(result).to.equal(2)
+    })
   })
 })
