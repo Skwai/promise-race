50a7db0efb64d0ad20c5a6eeaa174b16315b08fc
Cameron <cameron@biteable.com>
2017-12-07 16:38:16 +1100

    jsdoc

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
+/**
+ * Class representing a PromiseRace
+ */
 export default class PromiseRace {
+  /**
+   * Create a PromiseRace
+   */
   constructor () {
-    this.last = null
-    this.lastID = null
+    Object.assign(this, {
+      last: null,
+      lastID: null
+    })
   }
 
+  /**
+   * Returns true if the ID passed matches the ID of the last promise appended
+   * @return {Boolean}
+   */
   isLastID (id) {
     return id === this.lastID
   }
 
+  /**
+   * Create a unique identifier to identify each promise
+   * @return {String}
+   */
   createID () {
     const id = Math.random().toString(36).substr(2, 10)
     this.lastID = id
     return id
   }
 
+  /**
+   * Add a promise to the race
+   * @param {Promise} promise
+   * @return {Promise}
+   */
   append (promise) {
     if (!(promise instanceof Promise)) {
       throw Error('argument must be an instance of Promise')
@@ -32,6 +53,10 @@ export default class PromiseRace {
     return intercepted
   }
 
+  /**
+   * Return a promise that resolves when the last promise called fires
+   * @return {Promise}
+   */
   resolved () {
     return this.last || Promise.reject(Error('No promises have been received'))
   }
